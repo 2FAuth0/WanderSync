e1ea63d248a723d3474db101e2343d1e3034f456
varungiridhar <varungiridhar6@gmail.com>
2024-10-06 19:03:40 -0400

    Add methods for reading/writing data in the future - this can subsequently be moved to a common class that can be accessed by all other views.

--- WanderSync/app/src/main/java/com/example/wandersync/view/MainActivity.java
+++ WanderSync/app/src/main/java/com/example/wandersync/view/MainActivity.java
@@ -18,20 +18,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.wandersync.model.User;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import com.example.wandersync.R;
 import com.example.wandersync.viewmodel.LoginViewModel;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     // TODO: Abhi splash screen or welcome screen here
     private final String TAG = "MainActivity";
     private FirebaseAuth myAuth;
-  
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -106,5 +110,48 @@ public class MainActivity extends AppCompatActivity {
         } else {
             // User is signed out
         }
+    }
+
+    // Method to read data from Firebase Database
+    public void readUserData(String userId) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference userRef = database.getReference("users").child(userId);
+
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get User object and use the values to update the UI
+                User user = dataSnapshot.getValue(User.class);
+                if (user != null) {
+                    // Update UI with user data
+                    Toast.makeText(MainActivity.this, "Name: " + user.getName() + ", Email: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "User not found.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "Failed to read user data.", databaseError.toException());
+            }
+        });
+    }
+
+    // Method to write data to Firebase Database
+    public void writeUserData(String userId, String name, String email) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference usersRef = database.getReference("users");
+
+        // Create a user object (You can use your own model class here)
+        User user = new User(name, email);
+
+        // Write to the 'users/userId' node
+        usersRef.child(userId).setValue(user).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(MainActivity.this, "User data saved successfully!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Failed to save user data.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
